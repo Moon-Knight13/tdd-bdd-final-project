e2623174440c5ecd8f3856f116097d317dd6ae23
IBM_TDD_BDD <195570009+Moon-Knight13@users.noreply.github.com>
2026-03-18 13:58:01 -0400

    Exercise 1 Complete

--- tests/factories.py
+++ tests/factories.py
@@ -14,6 +14,10 @@
 
 # pylint: disable=too-few-public-methods
 
+# https://faker.readthedocs.io/en/master/providers/baseprovider.html
+
+# https://factoryboy.readthedocs.io/en/stable/fuzzy.html
+
 """
 Test Factory to make fake objects for testing
 """
@@ -31,4 +35,32 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+   ## Add code to create Fake Products   
+    name = factory.FuzzyChoice(
+        choices=[
+            "Hat",
+            "Pants",
+            "Shirt",
+            "Apple",
+            "Banana",
+            "Pots",
+            "Towels",
+            "Ford",
+            "Chevy",
+            "Hammer",
+            "Wrench"
+        ]
+    )
+    description = factory.faker("text")
+    price = factory.FuzzyDecimal(0.5, 2000.0, 2)
+    available = factory.FuzzyChoice(choices=[True, False])
+    category = factory.FuzzyChoice(
+        choices=[
+            Category.UNKNOWN,
+            Category.CLOTHS,
+            Category.FOOD,
+            Category.HOUSEWARES,
+            Category.AUTOMOTIVE
+        ]
+    )
+    
